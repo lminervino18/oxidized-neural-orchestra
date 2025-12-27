9056bdcdba9dd9ee0d78701ffbb93878110b3293
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-12-27 18:02:09 -0300

    feat: added serialization and deserialization for small Msg implementation

--- comms/src/msg.rs
+++ comms/src/msg.rs
@@ -5,41 +5,55 @@ use std::{
 
 use crate::{Deserialize, Serialize};
 
+#[derive(Debug)]
 pub enum Payload<'a> {
     Gradient(Cow<'a, [f32]>),
     Weights(Cow<'a, [f32]>),
 }
 
-pub enum ControlMsg {}
-
+#[derive(Debug)]
 pub enum Msg<'a> {
     Data(Payload<'a>),
-    Control(ControlMsg),
 }
 
 impl Deserialize for Msg<'_> {
     fn deserialize<R: Read>(reader: &mut R) -> io::Result<Self> {
-        todo!()
+        let mut kind_buf = [0; 1];
+        reader.read_exact(&mut kind_buf)?;
+        let kind = u8::from_be_bytes(kind_buf);
+
+        let mut len_buf = [0; 8];
+        reader.read_exact(&mut len_buf)?;
+        let len = u64::from_be_bytes(len_buf) as usize;
+
+        let mut nums = vec![0f32; len];
+        let data_buf = bytemuck::cast_slice_mut(&mut nums);
+        reader.read_exact(data_buf)?;
+
+        let payload = match kind {
+            0 => Payload::Gradient(Cow::Owned(nums)),
+            1 => Payload::Weights(Cow::Owned(nums)),
+            x => panic!("wrong message kind {x}"),
+        };
+
+        Ok(Msg::Data(payload))
     }
 }
 
 impl Serialize for Msg<'_> {
     fn serialize<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        match self {
-            Msg::Data(Payload::Gradient(grad)) => {
-                writer.write_all(&[0])?;
+        let (kind, nums) = match self {
+            Msg::Data(Payload::Gradient(grad)) => (0, grad),
+            Msg::Data(Payload::Weights(weights)) => (1, weights),
+        };
 
-                let len = grad.len() as u64;
-                writer.write_all(&len.to_be_bytes())?;
+        writer.write_all(&[kind])?;
 
-                let bytes = bytemuck::cast_slice(grad);
-                writer.write_all(&bytes)?;
-            }
-            Msg::Data(Payload::Weights(weights)) => {
-                writer.write(&[1])?;
-            }
-            Msg::Control(control_msg) => todo!(),
-        }
+        let len = nums.len() as u64;
+        writer.write_all(&len.to_be_bytes())?;
+
+        let bytes = bytemuck::cast_slice(nums);
+        writer.write_all(&bytes)?;
 
         Ok(())
     }
--- comms/src/proto.rs
+++ comms/src/proto.rs
@@ -1,4 +1,4 @@
-//! Implements a simple communication protocol, writes the length of the message first using 4 bytes.
+//! Implements a simple communication protocol, writes the length of the message first.
 
 use std::io::{self, Cursor};
 
@@ -6,32 +6,35 @@ use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use crate::{Deserialize, Serialize};
 
-/// Writes 4 bytes of length and the data into `sink` using `buf` for serialization.
+type LenType = u64;
+const LEN_SIZE: usize = size_of::<LenType>();
+
+/// Writes bytes of length and the data into `sink` using `buf` for serialization.
 pub async fn write_msg<T, W>(msg: &T, buf: &mut Vec<u8>, sink: &mut W) -> io::Result<()>
 where
     T: Serialize,
     W: AsyncWrite + Unpin,
 {
-    buf.resize(4, 0);
+    buf.resize(LEN_SIZE, 0);
 
     let start = buf.len();
     msg.serialize(buf)?;
     let end = buf.len();
 
-    let len = (end - start) as u32;
-    buf[..4].copy_from_slice(&len.to_be_bytes());
+    let len = (end - start) as LenType;
+    buf[..LEN_SIZE].copy_from_slice(&len.to_be_bytes());
     sink.write_all(buf).await
 }
 
-/// Reads 4 bytes of length and then the data, returns the deserialized item using `buf` for deserialization.
+/// Reads bytes of length and then the data, returns the deserialized item using `buf` for deserialization.
 pub async fn read_msg<T, R>(src: &mut R, buf: &mut Vec<u8>) -> io::Result<T>
 where
     T: Deserialize,
     R: AsyncRead + Unpin,
 {
-    let mut size = [0; 4];
+    let mut size = [0; LEN_SIZE];
     src.read_exact(&mut size).await?;
-    let len = u32::from_be_bytes(size) as usize;
+    let len = LenType::from_be_bytes(size) as usize;
 
     buf.resize(len, 0);
     src.read_exact(buf).await?;
