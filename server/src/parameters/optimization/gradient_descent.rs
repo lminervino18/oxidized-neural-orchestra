77d0da935dfe3307c6f75069c93c16ff6fc534bb
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-12-28 02:32:41 -0300

    feat: added gradient descent with momentum

--- server/src/parameters/optimization/gradient_descent.rs
+++ server/src/parameters/optimization/gradient_descent.rs
@@ -2,13 +2,21 @@ use super::Optimizer;
 
 #[derive(Debug)]
 pub struct GradientDescent {
-    lr: f32,
+    learning_rate: f32,
+}
+
+impl GradientDescent {
+    pub fn new(learning_rate: f32) -> Self {
+        Self { learning_rate }
+    }
 }
 
 impl Optimizer for GradientDescent {
     fn update_weights(&mut self, weights: &mut [f32], grad: &[f32]) {
+        let lr = self.learning_rate;
+
         for (w, g) in weights.iter_mut().zip(grad) {
-            *w -= self.lr * g;
+            *w -= lr * g;
         }
     }
 }
--- server/src/parameters/optimization/mod.rs
+++ server/src/parameters/optimization/mod.rs
@@ -1,5 +1,7 @@
 mod gradient_descent;
+mod gradient_descent_with_momentum;
 mod optimizer;
 
 pub use gradient_descent::GradientDescent;
+pub use gradient_descent_with_momentum::GradientDescentWithMomentum;
 pub use optimizer::Optimizer;
