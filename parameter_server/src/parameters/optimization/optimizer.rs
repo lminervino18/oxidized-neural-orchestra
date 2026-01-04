ca7290550dd6d335de27f829e67869ca7154385d
mbianchif <89595589+mbianchif@users.noreply.github.com>
2026-01-04 01:05:53 -0300

    refactor: updated some of the methods in the parameter_server module to be more alike and updated some of the docstrings too

--- parameter_server/src/parameters/optimization/optimizer.rs
+++ parameter_server/src/parameters/optimization/optimizer.rs
@@ -1,11 +1,9 @@
 /// Defines the strategy for updating model parameters based on calculated gradients.
-///
-/// The `Optimizer` trait is responsible for the mathematical transition of weights from state `t` to `t+1`.
 pub trait Optimizer {
     /// Updates the provided slice of weights using the accumulated gradients.
     ///
     /// # Arguments
-    /// * `grad` - The accumulated gradients corresponding to the `weights` slice.
-    /// * `weights` - A mutable slice of the current parameter values for a specific shard.
+    /// * `grad` - A reference to the model's gradient.
+    /// * `weights` - The weights to update.
     fn update_weights(&mut self, grad: &[f32], weights: &mut [f32]);
 }
