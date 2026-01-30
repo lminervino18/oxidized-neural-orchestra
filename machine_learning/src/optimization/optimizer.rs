2549de0e539f070639136a56d77a2e068ae8b971
Alejo Ordoñez <alordonez@fi.uba.ar>
2026-01-30 19:31:32 -0300

    doc: docstrings for gradient descent

--- machine_learning/src/optimization/optimizer.rs
+++ machine_learning/src/optimization/optimizer.rs
@@ -1,3 +1,4 @@
+/// This trait represents the *learning rule* that is to be taken when updating the parameters of a model.
 pub trait Optimizer {
     fn update_params(&mut self, params: &mut [f32], grad: &[f32]);
 }
