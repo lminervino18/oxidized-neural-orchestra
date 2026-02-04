e89d87cbfd8e24b9cfee967742a2267167200945
lminervino18 <lminervino@fi.uba.ar>
2026-02-04 16:56:19 -0300

    refactor: change server_addr for AlgorithSpec in WorkerSpec

--- comms/src/specs/worker/mod.rs
+++ comms/src/specs/worker/mod.rs
@@ -1,20 +1,15 @@
-use std::net::IpAddr;
-
 use serde::{Deserialize, Serialize};
 
 pub mod algorithm;
 pub mod model;
-pub mod training;
 
 use super::machine_learning::TrainerSpec;
 pub use algorithm::AlgorithmSpec;
-pub use model::{ActFnSpec, LayerSpec, ModelSpec};
-pub use training::TrainingSpec;
 
 /// Wire-level bootstrap specification for a worker instance.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct WorkerSpec {
     pub worker_id: usize,
     pub trainer: TrainerSpec,
-    pub server_addr: IpAddr,
+    pub algorithm: AlgorithmSpec,
 }
