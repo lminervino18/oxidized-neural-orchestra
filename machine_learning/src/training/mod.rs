c212892d0b577ab589090ca42bb6e635b0ba7529
mbianchif <89595589+mbianchif@users.noreply.github.com>
2026-02-13 23:45:30 -0300

    feat: added new parameter manager for the model trainer

--- machine_learning/src/training/mod.rs
+++ machine_learning/src/training/mod.rs
@@ -1,7 +1,9 @@
 mod builder;
 mod model_trainer;
+mod param_manager;
 mod trainer;
 
 pub use builder::TrainerBuilder;
 pub use model_trainer::ModelTrainer;
+pub use param_manager::ParamManager;
 pub use trainer::Trainer;
