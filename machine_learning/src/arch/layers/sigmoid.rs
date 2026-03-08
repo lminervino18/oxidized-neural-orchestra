cfb8f38d03f25665fdc5836d45cc17cb46d120e1
mbianchif <89595589+mbianchif@users.noreply.github.com>
2026-03-07 22:49:39 -0300

    feat: fixed the parameter manager such that empty layers don't shift both FrontIter's and BackIter's internal curr pointer

--- machine_learning/src/arch/layers/sigmoid.rs
+++ machine_learning/src/arch/layers/sigmoid.rs
@@ -39,60 +39,11 @@ impl Sigmoid {
         &'a mut self,
         mut d: ArrayViewMut2<'a, f32>,
     ) -> Result<ArrayViewMut2<'a, f32>> {
-        let amp = self.amp;
-
         azip!((d_in in &mut d, &a in &self.activations) {
-            let s = a / amp;
-            let local_grad = amp * s * (1.0 - s);
-            *d_in *= local_grad;
+            let s = a / self.amp;
+            *d_in *= a * (1.0 - s);
         });
 
         Ok(d)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_sigmoid_forward() {
-        let amp = f32::consts::PI;
-        let mut sigmoid = Sigmoid::new(amp);
-
-        /* x =  [9 10 11]
-         ***/
-        let x = ArrayView2::from_shape((1, 3), &[9.0, 10.0, 11.0]).unwrap();
-
-        /* sigmoid(x)
-         ***/
-        let expected = x.mapv(|z: f32| amp / (1.0 + (-z).exp()));
-
-        let activations = sigmoid.forward(x).unwrap();
-
-        assert_eq!(activations, expected);
-    }
-
-    #[test]
-    fn test_sigmoid_backward() {
-        let amp = f32::consts::PI;
-        let mut sigmoid = Sigmoid::new(amp);
-
-        /* x =  [9 10 11]
-         ***/
-        let mut d_raw = [9.0, 10.0, 11.0];
-        let d = ArrayViewMut2::from_shape((1, 3), &mut d_raw).unwrap();
-
-        /* sigmoid(x)
-         ***/
-        let s = |z: f32| 1.0 / (1.0 + (-z).exp());
-        let expected = d.mapv(|z| amp * s(z) * (1.0 - s(z)));
-
-        let activations = sigmoid.backward(d).unwrap();
-
-        activations
-            .iter()
-            .zip(expected)
-            .for_each(|(a, e)| assert!(f32::abs(a - e) < f32::EPSILON));
     }
 }
