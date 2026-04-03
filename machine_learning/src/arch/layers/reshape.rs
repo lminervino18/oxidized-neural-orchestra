50b77b0ad3b57a17e900d165a8f162eda6cc50a6
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-04-03 18:36:52 -0300

    refactor(ml): avoid having a reshape layer buffer and reshaping it

--- machine_learning/src/arch/layers/reshape.rs
+++ machine_learning/src/arch/layers/reshape.rs
@@ -1,14 +1,17 @@
-use ndarray::{
-    Array, ArrayBase, ArrayD, ArrayView2, ArrayView4, ArrayViewD, ArrayViewMut2, ArrayViewMut4,
-    ArrayViewMutD, Data, Dimension, Ix2, Ix4, IxDyn,
-};
+use ndarray::{ArrayView2, ArrayView4, ArrayViewD, ArrayViewMut2, ArrayViewMut4, ArrayViewMutD};
 
-use crate::{MlErr, Result, arch::InplaceReshape};
+use crate::{MlErr, Result};
 
 /// The metadata of a `Reshape` layer.
 #[derive(Clone)]
-pub struct Reshape2 {
-    buf: ArrayD<f32>,
+pub struct TwoDTo4D {
+    channels: usize,
+    height: usize,
+    width: usize,
+}
+
+#[derive(Clone)]
+pub struct FourDTo2D {
     channels: usize,
     height: usize,
     width: usize,
@@ -17,16 +20,13 @@ pub struct Reshape2 {
 /// A `Reshape` layer.
 #[derive(Clone)]
 pub enum Reshape {
-    TwoDTo4D(Reshape2),
-    FourDTo2D(Reshape2),
+    TwoDTo4D(TwoDTo4D),
+    FourDTo2D(FourDTo2D),
 }
 
 impl Reshape {
     pub fn two_d_to4d(channels: usize, height: usize, width: usize) -> Self {
-        let zeros = ArrayD::zeros(IxDyn(&[0]));
-
-        Self::TwoDTo4D(Reshape2 {
-            buf: zeros,
+        Self::TwoDTo4D(TwoDTo4D {
             channels,
             height,
             width,
@@ -34,10 +34,7 @@ impl Reshape {
     }
 
     pub fn four_d_to2d(channels: usize, height: usize, width: usize) -> Self {
-        let zeros = ArrayD::zeros(IxDyn(&[0]));
-
-        Self::FourDTo2D(Reshape2 {
-            buf: zeros,
+        Self::FourDTo2D(FourDTo2D {
             channels,
             height,
             width,
@@ -55,20 +52,15 @@ impl Reshape {
     ///
     /// # Returns
     /// The tensor with its dimensionality converted to match the layer's output.
-    pub fn forward<'a>(&'a mut self, input: ArrayViewD<f32>) -> Result<ArrayViewD<'a, f32>> {
+    pub fn forward<'a>(&self, input: ArrayViewD<'a, f32>) -> Result<ArrayViewD<'a, f32>> {
         match self {
             Self::TwoDTo4D(reshape) => {
-                let input = input.into_dimensionality::<Ix2>().unwrap();
-                reshape.two_d_to_4d(input)?;
-                let reshaped = reshape.two_d_to_4d(input.view())?;
-
-                Ok(reshaped.into_dyn())
+                let arr = input.into_dimensionality().unwrap();
+                Ok(reshape.forward(arr)?.into_dyn())
             }
             Self::FourDTo2D(reshape) => {
-                let input = input.into_dimensionality().unwrap();
-                let reshaped = reshape.four_d_to_2d(input)?;
-
-                Ok(reshaped.into_dyn())
+                let arr = input.into_dimensionality().unwrap();
+                Ok(reshape.forward(arr)?.into_dyn())
             }
         }
     }
@@ -80,47 +72,32 @@ impl Reshape {
     ///
     /// # Returns
     /// The tensor with its dimensionality converted to match the layer's input.
-    pub fn backward<'a>(&'a mut self, delta: ArrayViewMutD<f32>) -> Result<ArrayViewMutD<'a, f32>> {
+    pub fn backward<'a>(&self, delta: ArrayViewMutD<'a, f32>) -> Result<ArrayViewMutD<'a, f32>> {
         match self {
             Self::TwoDTo4D(reshape) => {
-                let delta = delta.into_dimensionality().unwrap();
-                let reshaped = reshape.four_d_to_2d_mut(delta.view())?;
-
-                Ok(reshaped.into_dyn())
+                let arr = delta.into_dimensionality().unwrap();
+                Ok(reshape.backward(arr)?.into_dyn())
             }
             Self::FourDTo2D(reshape) => {
-                let delta = delta.into_dimensionality().unwrap();
-                let reshaped = reshape.two_d_to_4d_mut(delta.view())?;
-
-                Ok(reshaped.into_dyn())
+                let arr = delta.into_dimensionality().unwrap();
+                Ok(reshape.backward(arr)?.into_dyn())
             }
         }
     }
 }
 
-/* Finally went with the reshape buffer
- * TODO: avoid code repetition, the problem here is that there doesn't exist a "downgrade" method
- * for converting an array view mut into just a view, but there's probably still code that is
- * unecessary dupped
- * TODO: maybe check the other solution: having the dataset split its sample and label memory
- * ***/
-impl Reshape2 {
-    fn two_d_to_4d<'a>(&'a mut self, arr: ArrayView2<f32>) -> Result<ArrayView4<'a, f32>>
+impl TwoDTo4D {
+    fn forward<'a>(&self, arr: ArrayView2<'a, f32>) -> Result<ArrayView4<'a, f32>>
 where {
-        let Reshape2 {
-            ref mut buf,
+        let Self {
             channels,
             height,
             width,
         } = *self;
         let batch_size = arr.dim().0;
 
-        buf.reshape_inplace(arr.raw_dim().into_dyn());
-        buf.assign(&arr);
-
         let arr_size = arr.dim().1;
-        buf.view()
-            .into_shape_with_order((batch_size, channels, height, width))
+        arr.into_shape_with_order((batch_size, channels, height, width))
             .map_err(|_| MlErr::SizeMismatch {
                 what: "2d array to 4d array",
                 got: arr_size,
@@ -128,70 +105,57 @@ where {
             })
     }
 
-    fn four_d_to_2d<'a>(&'a mut self, arr: ArrayView4<f32>) -> Result<ArrayView2<'a, f32>>
+    fn backward<'a>(&self, arr: ArrayViewMut4<'a, f32>) -> Result<ArrayViewMut2<'a, f32>>
 where {
-        let Reshape2 {
-            ref mut buf,
+        let Self {
             channels,
             height,
             width,
         } = *self;
         let batch_size = arr.dim().0;
 
-        buf.reshape_inplace(arr.raw_dim().into_dyn());
-        buf.assign(&arr);
+        let arr_size = arr.dim().2 * arr.dim().3;
+        arr.into_shape_with_order((batch_size, channels * height * width))
+            .map_err(|_| MlErr::SizeMismatch {
+                what: "4d array to 2d array in backward",
+                got: arr_size,
+                expected: height * width,
+            })
+    }
+}
+
+impl FourDTo2D {
+    fn forward<'a>(&self, arr: ArrayView4<'a, f32>) -> Result<ArrayView2<'a, f32>>
+where {
+        let Self {
+            channels,
+            height,
+            width,
+        } = *self;
+        let batch_size = arr.dim().0;
 
         let arr_size = arr.dim().2 * arr.dim().3;
-        buf.view()
-            .into_shape_with_order((batch_size, channels * height * width))
+        arr.into_shape_with_order((batch_size, channels * height * width))
             .map_err(|_| MlErr::SizeMismatch {
-                what: "4d array to 2d array",
+                what: "4d array to 2d array in forward",
                 got: arr_size,
                 expected: height * width,
             })
     }
 
-    fn two_d_to_4d_mut<'a>(&'a mut self, arr: ArrayView2<f32>) -> Result<ArrayViewMut4<'a, f32>>
+    fn backward<'a>(&self, arr: ArrayViewMut2<'a, f32>) -> Result<ArrayViewMut4<'a, f32>>
 where {
-        let Reshape2 {
-            ref mut buf,
+        let Self {
             channels,
             height,
             width,
         } = *self;
         let batch_size = arr.dim().0;
-
-        buf.reshape_inplace(arr.raw_dim().into_dyn());
-        buf.assign(&arr);
 
         let arr_size = arr.dim().1;
-        buf.view_mut()
-            .into_shape_with_order((batch_size, channels, height, width))
+        arr.into_shape_with_order((batch_size, channels, height, width))
             .map_err(|_| MlErr::SizeMismatch {
-                what: "2d array to 4d array",
-                got: arr_size,
-                expected: height * width,
-            })
-    }
-
-    fn four_d_to_2d_mut<'a>(&'a mut self, arr: ArrayView4<f32>) -> Result<ArrayViewMut2<'a, f32>>
-where {
-        let Reshape2 {
-            ref mut buf,
-            channels,
-            height,
-            width,
-        } = *self;
-        let batch_size = arr.dim().0;
-
-        buf.reshape_inplace(arr.raw_dim().into_dyn());
-        buf.assign(&arr);
-
-        let arr_size = arr.dim().2 * arr.dim().3;
-        buf.view_mut()
-            .into_shape_with_order((batch_size, channels * height * width))
-            .map_err(|_| MlErr::SizeMismatch {
-                what: "4d array to 2d array",
+                what: "2d array to 4d array in backward",
                 got: arr_size,
                 expected: height * width,
             })
@@ -208,16 +172,16 @@ mod tests {
         let channels = 1;
         let height = 2;
         let width = 2;
-        let mut reshape = Reshape::two_d_to4d(channels, height, width);
+        let reshape = Reshape::two_d_to4d(channels, height, width);
 
-        let input = array![[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]];
+        let input = array![[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]].into_dyn();
         let expected = array![
             [[[1.0, 2.0], [3.0, 4.0]]],
             [[[5.0, 6.0], [7.0, 8.0]]],
             [[[9.0, 10.0], [11.0, 12.0]]]
         ]
         .into_dyn();
-        let output = reshape.forward(input.into_dyn().view()).unwrap();
+        let output = reshape.forward(input.view()).unwrap();
 
         assert_eq!(output, expected);
     }
@@ -227,16 +191,16 @@ mod tests {
         let channels = 1;
         let height = 2;
         let width = 2;
-        let mut reshape = Reshape::two_d_to4d(channels, height, width);
+        let reshape = Reshape::two_d_to4d(channels, height, width);
 
-        let input = array![
+        let mut input = array![
             [[[1.0, 2.0], [3.0, 4.0]]],
             [[[5.0, 6.0], [7.0, 8.0]]],
             [[[9.0, 10.0], [11.0, 12.0]]]
         ]
         .into_dyn();
         let expected = array![[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]].into_dyn();
-        let output = reshape.backward(input.into_dyn().view_mut()).unwrap();
+        let output = reshape.backward(input.view_mut()).unwrap();
 
         assert_eq!(output, expected);
     }
@@ -246,7 +210,7 @@ mod tests {
         let channels = 1;
         let height = 2;
         let width = 2;
-        let mut reshape = Reshape::four_d_to2d(channels, height, width);
+        let reshape = Reshape::four_d_to2d(channels, height, width);
 
         let input = array![
             [[[1.0, 2.0], [3.0, 4.0]]],
@@ -255,7 +219,7 @@ mod tests {
         ]
         .into_dyn();
         let expected = array![[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]].into_dyn();
-        let output = reshape.forward(input.into_dyn().view()).unwrap();
+        let output = reshape.forward(input.view()).unwrap();
 
         assert_eq!(output, expected);
     }
@@ -265,17 +229,66 @@ mod tests {
         let channels = 1;
         let height = 2;
         let width = 2;
-        let mut reshape = Reshape::four_d_to2d(channels, height, width);
+        let reshape = Reshape::four_d_to2d(channels, height, width);
 
-        let input = array![[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]];
+        let mut input = array![[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.]].into_dyn();
         let expected = array![
             [[[1.0, 2.0], [3.0, 4.0]]],
             [[[5.0, 6.0], [7.0, 8.0]]],
             [[[9.0, 10.0], [11.0, 12.0]]]
         ]
         .into_dyn();
-        let output = reshape.backward(input.into_dyn().view_mut()).unwrap();
+        let output = reshape.backward(input.view_mut()).unwrap();
 
         assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_reshape_two_d_to4d_forward_consecutive() {
+        let channels = 1;
+        let height = 2;
+        let width = 2;
+        let reshape = Reshape::two_d_to4d(channels, height, width);
+
+        let input1 = array![[1., 2., 3., 4.]].into_dyn();
+        let expected1 = array![[[[1., 2.], [3., 4.]]]].into_dyn();
+        let output1 = reshape.forward(input1.view()).unwrap();
+
+        assert_eq!(output1, expected1);
+
+        let input2 = array![[1., 2., 3., 4.], [5., 6., 7., 8.],].into_dyn();
+        let expected2 = array![[[[1., 2.], [3., 4.]]], [[[5., 6.], [7., 8.]]],].into_dyn();
+        let output2 = reshape.forward(input2.view()).unwrap();
+
+        assert_eq!(output2, expected2);
+
+        let input3 = array![[1., 2., 3., 4.], [5., 6., 7., 8.], [9., 10., 11., 12.],].into_dyn();
+        let expected3 = array![
+            [[[1., 2.], [3., 4.]]],
+            [[[5., 6.], [7., 8.]]],
+            [[[9., 10.], [11., 12.]]],
+        ]
+        .into_dyn();
+        let output3 = reshape.forward(input3.view()).unwrap();
+
+        assert_eq!(output3, expected3);
+
+        let input4 = array![
+            [1., 2., 3., 4.],
+            [5., 6., 7., 8.],
+            [9., 10., 11., 12.],
+            [13., 14., 15., 16.]
+        ]
+        .into_dyn();
+        let expected4 = array![
+            [[[1., 2.], [3., 4.]]],
+            [[[5., 6.], [7., 8.]]],
+            [[[9., 10.], [11., 12.]]],
+            [[[13., 14.], [15., 16.]]]
+        ]
+        .into_dyn();
+        let output4 = reshape.forward(input4.view()).unwrap();
+
+        assert_eq!(output4, expected4);
     }
 }
