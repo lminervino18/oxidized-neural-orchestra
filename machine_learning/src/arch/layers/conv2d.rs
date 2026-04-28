813b11533937cbf6e0905aca8288300000df3b92
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-04-27 23:23:34 -0300

    test: conv2d forward with multiple param combinations

--- machine_learning/src/arch/layers/conv2d.rs
+++ machine_learning/src/arch/layers/conv2d.rs
@@ -321,30 +321,6 @@ mod tests {
     }
 
     #[test]
-    fn test_conv2d_forward() {
-        let filters = 1;
-        let in_channels = 1;
-        let kernel_size = 2;
-        let stride = 2;
-        let padding = 0;
-        let mut conv = Conv2d::new(filters, in_channels, kernel_size, stride, padding);
-
-        let input: Array4<f32> = array![[[
-            [1., 2., 3., 4.],
-            [5., 6., 7., 8.],
-            [9., 10., 11., 12.],
-            [13., 14., 15., 16.]
-        ]]];
-
-        let params: [f32; 5] = [1., 2., 3., 4., 5.];
-        let expected_output = array![[[[49., 69.], [129., 149.]]]];
-
-        let output = conv.forward(&params[..], input.view()).unwrap();
-
-        assert_eq!(output, expected_output);
-    }
-
-    #[test]
     fn test_dilate_and_pad() {
         let filters = 1;
         let in_channels = 1;
@@ -376,6 +352,183 @@ mod tests {
         let got = conv.dilated;
 
         assert_eq!(got, expected);
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    fn test_conv2d_forward(
+        filters: usize,
+        in_channels: usize,
+        kernel_size: usize,
+        stride: usize,
+        padding: usize,
+        params: &[f32],
+        input: &Array4<f32>,
+        expected: &Array4<f32>,
+    ) {
+        let mut conv = Conv2d::new(filters, in_channels, kernel_size, stride, padding);
+        let output = conv.forward(params, input.view()).unwrap();
+        println!("output:\n{:#}", output);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_conv2d_forward_one_filter_one_in_channel_kernel_size2_one_stride_no_padding() {
+        let params: [f32; 5] = [
+            1., 2., 3., 4., // filter
+            5., // bias
+        ];
+        let input: Array4<f32> = array![[[[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]]];
+        let expected = array![
+            // first sample
+            [
+                // first input channel
+                [[42., 52.], [72., 82.]]
+            ]
+        ];
+        test_conv2d_forward(1, 1, 2, 1, 0, &params, &input, &expected);
+    }
+
+    #[test]
+    fn test_conv2d_forward_one_filter_one_in_channel_kernel_size2_one_stride_padding1() {
+        let params: [f32; 5] = [
+            1., 2., 3., 4., // filter
+            5., // bias
+        ];
+        let input: Array4<f32> = array![
+            // first sample
+            [
+                // first input channel
+                [[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]
+            ]
+        ];
+        let expected = array![
+            // first convoluted sample
+            [
+                // first convoluted in channel
+                [
+                    [9., 16., 23., 14.],
+                    [23., 42., 52., 26.],
+                    [41., 72., 82., 38.],
+                    [19., 28., 31., 14.]
+                ]
+            ]
+        ];
+        test_conv2d_forward(1, 1, 2, 1, 1, &params, &input, &expected);
+    }
+
+    #[test]
+    fn test_conv2d_forward_filters1_in_channels1_kernel_size2_one_stride_padding1_batch_size2() {
+        let params: [f32; 5] = [
+            1., 2., 3., 4., // filter
+            5., // bias
+        ];
+        let input: Array4<f32> = array![
+            [[[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]],
+            [[[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]]
+        ];
+        let expected = array![
+            [[
+                [9., 16., 23., 14.],
+                [23., 42., 52., 26.],
+                [41., 72., 82., 38.],
+                [19., 28., 31., 14.]
+            ]],
+            [[
+                [9., 16., 23., 14.],
+                [23., 42., 52., 26.],
+                [41., 72., 82., 38.],
+                [19., 28., 31., 14.]
+            ]]
+        ];
+        test_conv2d_forward(1, 1, 2, 1, 1, &params, &input, &expected);
+    }
+
+    #[test]
+    fn test_conv2d_forward_filters2_in_channels1_kernel_size2_one_stride_padding1_batch_size2() {
+        let params: [f32; 10] = [1., 2., 3., 4., 1., 2., 3., 4., 5., 5.];
+        let input: Array4<f32> = array![
+            [[[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]],
+            [[[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]]
+        ];
+        let expected = array![
+            [
+                [
+                    [9., 16., 23., 14.],
+                    [23., 42., 52., 26.],
+                    [41., 72., 82., 38.],
+                    [19., 28., 31., 14.]
+                ],
+                [
+                    [9., 16., 23., 14.],
+                    [23., 42., 52., 26.],
+                    [41., 72., 82., 38.],
+                    [19., 28., 31., 14.]
+                ]
+            ],
+            [
+                [
+                    [9., 16., 23., 14.],
+                    [23., 42., 52., 26.],
+                    [41., 72., 82., 38.],
+                    [19., 28., 31., 14.]
+                ],
+                [
+                    [9., 16., 23., 14.],
+                    [23., 42., 52., 26.],
+                    [41., 72., 82., 38.],
+                    [19., 28., 31., 14.]
+                ]
+            ]
+        ];
+        test_conv2d_forward(2, 1, 2, 1, 1, &params, &input, &expected);
+    }
+
+    #[test]
+    fn test_conv2d_forward_filters2_in_channels2_kernel_size2_one_stride_padding1_batch_size2() {
+        let params: [f32; 18] = [
+            1., 2., 3., 4., 1., 2., 3., 4., 1., 2., 3., 4., 1., 2., 3., 4., 5., 5.,
+        ];
+        let input: Array4<f32> = array![
+            [
+                [[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]],
+                [[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]
+            ],
+            [
+                [[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]],
+                [[1., 2., 3.,], [4., 5., 6.], [7., 8., 9.]]
+            ]
+        ];
+        let expected = array![
+            [
+                [
+                    [13.0, 27.0, 41.0, 23.0],
+                    [41.0, 79.0, 99.0, 47.0],
+                    [77.0, 139.0, 159.0, 71.0],
+                    [33.0, 51.0, 57.0, 23.0]
+                ],
+                [
+                    [13.0, 27.0, 41.0, 23.0],
+                    [41.0, 79.0, 99.0, 47.0],
+                    [77.0, 139.0, 159.0, 71.0],
+                    [33.0, 51.0, 57.0, 23.0]
+                ]
+            ],
+            [
+                [
+                    [13.0, 27.0, 41.0, 23.0],
+                    [41.0, 79.0, 99.0, 47.0],
+                    [77.0, 139.0, 159.0, 71.0],
+                    [33.0, 51.0, 57.0, 23.0]
+                ],
+                [
+                    [13.0, 27.0, 41.0, 23.0],
+                    [41.0, 79.0, 99.0, 47.0],
+                    [77.0, 139.0, 159.0, 71.0],
+                    [33.0, 51.0, 57.0, 23.0]
+                ]
+            ]
+        ];
+        test_conv2d_forward(2, 2, 2, 1, 1, &params, &input, &expected);
     }
 
     #[test]
